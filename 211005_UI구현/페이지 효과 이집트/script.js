9c267f03b7a3c004155618a75bfeb4852ae86e74
clrclrvhrvhr413 <clrclrvhrvhr413@gmail.com>
2021-10-13 13:31:37 +0900

    메뉴이벤트, css 정리
    메뉴 클릭하면
    해당 메뉴의 css 변화,
    4등분 되어 보이던 콘텐츠들이 좌/우로 사라지고,
    해당 메뉴가 가리키는 콘텐츠의 크기가 넓어진다.
    
    기능별로 css 위치 수정

--- 211005_UI구현/페이지 효과 이집트/script.js
+++ 211005_UI구현/페이지 효과 이집트/script.js
@@ -1,3 +1,15 @@
 $(function(){
 	$('#container').addClass('start');
-})
+
+	$('nav li').click(function(){
+		$('nav li').removeClass('on');
+		$(this).addClass('on');
+
+		$('#container').css('max-width','100%');
+		var id = $(this).attr('data-role');
+
+		$('.content').removeClass('prev this next');
+		$(`#${id}`).addClass('this').prevAll().addClass('prev');
+		$(`#${id}`).addClass('this').nextAll().addClass('next');
+	});
+});
